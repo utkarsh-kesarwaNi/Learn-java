b243ef5af4684b638e7c2cfc65a7d0aae798b8a5
utkarsh-kesarwaNi <utkarshk.web74@gmail.com>
2026-05-26 07:25:41 +0530

    forEachOrdered

--- src/com/uk/parallelstreams/ParallelStream.java
+++ src/com/uk/parallelstreams/ParallelStream.java
@@ -9,16 +9,21 @@ import java.util.List;
 public class ParallelStream {
     public static void main() {
 
+        System.out.println(Runtime.getRuntime().availableProcessors());
+
         long startTimeSequentialStream = System.currentTimeMillis();
-        ParallelStream.getStudentList().forEach(System.out::println);
+        ParallelStream.getStudentList().stream().filter(student -> student.getStudentAge() > 30).forEach(System.out::println);
         long endTimeSequentialStream = System.currentTimeMillis();
         System.out.println("Time taken by sequential stream : " + (endTimeSequentialStream - startTimeSequentialStream));
 
         long startTimeParallelStream = System.currentTimeMillis();
-//        Multiple threads are created automatically
-        ParallelStream.getStudentList().stream().parallel().forEach(System.out::println);
+//        Multiple threads are created automatically using Fork and join  framework
+        ParallelStream.getStudentList().stream().parallel().filter(student -> student.getStudentAge() > 30).forEach(System.out::println);
         long endTimeParallelStream = System.currentTimeMillis();
         System.out.println("Time taken by parallel stream : " + (endTimeParallelStream - startTimeParallelStream));
+
+//        forEachOrdered() performs an action on each element in the encounter order
+        ParallelStream.getStudentList().stream().parallel().filter(student -> student.getStudentAge() > 30).forEachOrdered(System.out::println);
     }
 
     public static List<Student> getStudentList() {
